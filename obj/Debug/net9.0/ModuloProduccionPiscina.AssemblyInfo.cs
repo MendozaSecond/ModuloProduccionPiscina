26ecc7308f06b5d0a933d07ee67956f8666e47ed
Edwin Mendoza <edwin2000st@gmail.com>
2025-07-08 14:39:14 -0500

    Actualizacion para crear pedidos

--- obj/Debug/net9.0/ModuloProduccionPiscina.AssemblyInfo.cs
+++ obj/Debug/net9.0/ModuloProduccionPiscina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloProduccionPiscina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4da5efc905b76bc5c2bb4002559d1e2d8d843f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloProduccionPiscina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloProduccionPiscina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
